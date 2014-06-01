2c223cf0f5b5f286f0cb24c76a7410bef6bbcdf5
Vinimoraesrc <vmrc@cin.ufpe.br>
2014-06-01 00:33:42 -0300

    Criacao da tela de pesquisa 50% concluida.

--- gen/br/com/JoinAndPlay/R.java
+++ gen/br/com/JoinAndPlay/R.java
@@ -22,38 +22,45 @@ public final class R {
         public static final int white=0x7f040001;
     }
     public static final class drawable {
-        public static final int campo_cinza=0x7f020000;
-        public static final int campo_esporte=0x7f020001;
-        public static final int ib_cal=0x7f020002;
-        public static final int ib_local=0x7f020003;
-        public static final int ib_perfil=0x7f020004;
-        public static final int ib_pesq=0x7f020005;
-        public static final int ic_cal=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int ic_local=0x7f020008;
-        public static final int ic_perfil=0x7f020009;
-        public static final int ic_pesq=0x7f02000a;
-        public static final int item_list=0x7f02000b;
-        public static final int linha=0x7f02000c;
-        public static final int linha2=0x7f02000d;
-        public static final int select_button=0x7f02000e;
-        public static final int seletc_tab=0x7f02000f;
-        public static final int seletc_wid_tab=0x7f020010;
-        public static final int tab_cal=0x7f020011;
-        public static final int tab_lista=0x7f020012;
-        public static final int tab_perfil=0x7f020013;
-        public static final int tab_pesq=0x7f020014;
+        public static final int campo_branco=0x7f020000;
+        public static final int campo_cinza=0x7f020001;
+        public static final int campo_esporte=0x7f020002;
+        public static final int ib_cal=0x7f020003;
+        public static final int ib_local=0x7f020004;
+        public static final int ib_perfil=0x7f020005;
+        public static final int ib_pesq=0x7f020006;
+        public static final int ic_cal=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int ic_local=0x7f020009;
+        public static final int ic_perfil=0x7f02000a;
+        public static final int ic_pesq=0x7f02000b;
+        public static final int item_list=0x7f02000c;
+        public static final int linha=0x7f02000d;
+        public static final int linha2=0x7f02000e;
+        public static final int select_button=0x7f02000f;
+        public static final int seletc_tab=0x7f020010;
+        public static final int seletc_wid_tab=0x7f020011;
+        public static final int tab_cal=0x7f020012;
+        public static final int tab_lista=0x7f020013;
+        public static final int tab_perfil=0x7f020014;
+        public static final int tab_pesq=0x7f020015;
     }
     public static final class id {
         public static final int H_scroll=0x7f080000;
-        public static final int RelativeLayout1=0x7f08001e;
+        public static final int RelativeLayout1=0x7f080028;
         public static final int TextView03=0x7f08000a;
         public static final int button1=0x7f080007;
-        public static final int button2=0x7f08001d;
-        public static final int content=0x7f080023;
-        public static final int editText1=0x7f08001a;
-        public static final int editText2=0x7f08001c;
-        public static final int handle=0x7f080024;
+        public static final int content=0x7f08002d;
+        public static final int escolha_endereco=0x7f080026;
+        public static final int escolha_endereco_layout=0x7f080025;
+        public static final int escolha_esporte=0x7f08001d;
+        public static final int escolha_esporte_layout=0x7f08001c;
+        public static final int esporte1=0x7f080020;
+        public static final int esporte2=0x7f080021;
+        public static final int esporte3=0x7f080022;
+        public static final int esporte4=0x7f080023;
+        public static final int esportes=0x7f08001f;
+        public static final int handle=0x7f08002e;
         public static final int imageView1=0x7f080005;
         public static final int imageView2=0x7f08000f;
         public static final int imageView3=0x7f080010;
@@ -72,21 +79,25 @@ public final class R {
         public static final int linearLayout1=0x7f080014;
         public static final int linearLayout2=0x7f08000c;
         public static final int listView1=0x7f080006;
-        public static final int menu_settings=0x7f080029;
+        public static final int local=0x7f080024;
+        public static final int main_layout=0x7f08001a;
+        public static final int menu_settings=0x7f080033;
+        public static final int my_location=0x7f080027;
         public static final int pager=0x7f080001;
-        public static final int progressBar1=0x7f080025;
-        public static final int progressBar2=0x7f080026;
+        public static final int pesquisar=0x7f08001e;
+        public static final int progressBar1=0x7f08002f;
+        public static final int progressBar2=0x7f080030;
         public static final int rightPane=0x7f080004;
-        public static final int scrollView1=0x7f080028;
-        public static final int seekBar1=0x7f080027;
-        public static final int slidingDrawer1=0x7f080022;
+        public static final int scrollView1=0x7f080032;
+        public static final int seekBar1=0x7f080031;
+        public static final int slidingDrawer1=0x7f08002c;
         public static final int sliding_pane_layout=0x7f080002;
-        public static final int spinner1=0x7f080020;
-        public static final int tableRow1=0x7f080021;
+        public static final int spinner1=0x7f08002a;
+        public static final int tableRow1=0x7f08002b;
         public static final int tabmain=0x7f080019;
-        public static final int textView1=0x7f08001b;
-        public static final int toggleButton1=0x7f08001f;
+        public static final int toggleButton1=0x7f080029;
         public static final int view1=0x7f08000e;
+        public static final int white_field_layout=0x7f08001b;
     }
     public static final class layout {
         public static final int activity_tab_tela=0x7f030000;
@@ -105,6 +116,9 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int hint_pesq1=0x7f050003;
+        public static final int hint_pesq2=0x7f050004;
+        public static final int local=0x7f050005;
         public static final int menu_settings=0x7f050002;
     }
     public static final class style {
