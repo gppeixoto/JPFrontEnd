8e4efc3dd932af411b6003e4fa7baac10e2869f6
Vinimoraesrc <vmrc@cin.ufpe.br>
2014-07-19 17:44:48 -0300

    Mais alterações na tela de pesquisa.

--- src/br/com/JoinAndPlay/PesquisarEventosFragment.java
+++ src/br/com/JoinAndPlay/PesquisarEventosFragment.java
@@ -1,12 +1,11 @@
 package br.com.JoinAndPlay;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
-
 
 public class PesquisarEventosFragment extends Fragment {
 	@Override
@@ -14,28 +13,45 @@ public class PesquisarEventosFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View v=inflater.inflate(R.layout.pesquisa_fragment, container,false);
 
-		ImageButton btn = (ImageButton) v.findViewById(R.id.pesquisar);
-
-		btn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				((MainActivity)getActivity()).mudarAba(0);
-			}
-		});
-
 		Button bg = (Button) v.findViewById(R.id.bigButton);
 		bg.setText("Pesquisar");
+		
+		Button b2 = (Button) v.findViewById(R.id.buttonDataInicio);
+		b2.setText("Início");
+		
+		Button b3 = (Button) v.findViewById(R.id.buttonDataFim);
+		b3.setText("Fim");
 		
 		bg.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Button b = (Button) v.findViewById(R.id.bigButton);
+				((MainActivity)getActivity()).mudarAba(0);
+				//Necessário obter especificação da designer sobre animações ao clicar nos botões.
+				//Button b = (Button) v.findViewById(R.id.bigButton);
 				//b.setTextColor(getResources().getColor(R.color.red));
 				//b.setTextColor(getResources().getColor(R.color.white));
 			}
 		});
 		
+		b2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showTimePickerDialog(v);
+			}
+		});
+		
+		b3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showTimePickerDialog(v);
+			}
+		});
+		
 		return v;
 	}
-	
+
+	public void showTimePickerDialog(View v) {
+	    DialogFragment newFragment = new TimePickerFragment();
+	    newFragment.show(getFragmentManager(), "timePicker");
+	}
 }
