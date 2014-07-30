e8cd67e12d62c1bfe252512a281f449b3c326310
jlam <jlam@G4C46.windows.cin.ufpe.br>
2014-07-30 10:20:43 -0300

    add bibliotecanova

--- src/br/com/JoinAndPlay/Event/EventFragment.java
+++ src/br/com/JoinAndPlay/Event/EventFragment.java
@@ -24,6 +24,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import org.joda.time.DateTime;
 
 public class EventFragment extends Fragment implements OnClickListener{
 	private ItemEvent myEvent;
