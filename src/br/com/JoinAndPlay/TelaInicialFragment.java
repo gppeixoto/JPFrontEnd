5d2b4dd0bdb531296269bb4494ff4019a7221468
jlam-jefferson <jlam@cin.ufpe.br>
2014-08-17 01:32:32 -0300

    tela inicial com scroll,tela de criar evento: dialogos funcionando e pesquisa de enderecos sem bug

--- src/br/com/JoinAndPlay/TelaInicialFragment.java
+++ src/br/com/JoinAndPlay/TelaInicialFragment.java
@@ -1,27 +1,18 @@
 package br.com.JoinAndPlay;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import com.facebook.Request;
-import com.facebook.Response;
 import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.Request.GraphUserCallback;
-import com.facebook.model.GraphUser;
 
 import br.com.JoinAndPlay.Server.Connecter;
 import br.com.JoinAndPlay.Server.Server;
 import br.com.JoinAndPlay.Server.Usuario;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class TelaInicialFragment extends Fragment implements Connecter<Usuario>, OnClickListener,Runnable{
 	private Button inicial_button_facebookLogin;
@@ -33,7 +24,7 @@ public class TelaInicialFragment extends Fragment implements Connecter<Usuario>,
 		View view = inflater.inflate(R.layout.layout_inicial, container,false);
 		inicial_button_facebookLogin = (Button) view.findViewById(R.id.inicial_button_facebook_login);
 		inicial_button_facebookLogin.setOnClickListener(this);
-view.post(this);
+		view.post(this);
 		return view;
 	}
 
@@ -52,22 +43,27 @@ view.post(this);
 
 	@Override
 	public void onClick(View v) {
-				Server.user_profile(getActivity(), this);
+		Server.user_profile(getActivity(), this);
 	}
 	int i=0;
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		if(getView()!=null){
-			if(Session.getActiveSession()!=null && Session.getActiveSession().isOpened() ){
-				((MainActivity)getActivity()).login();
-			
+			if(Session.getActiveSession()!=null ){
+
+				if(Session.getActiveSession().isOpened()){
+					((MainActivity)getActivity()).login();
+				}else{
+					onClick(null);
+				}
+
 				return;
 			}
 			getView().postDelayed(this, i*10);
 			if(i<1000)i++;
 		}
-		
+
 	}
 
 }
